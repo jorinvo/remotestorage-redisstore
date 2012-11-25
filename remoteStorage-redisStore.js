6195ad0c4cd925eed4a23fdc6ddbe7a599655457
Jorin Vogel <jorin.vogel@gmail.com>
2012-11-25 17:18:31 +0100

    use redis-url

--- remoteStorage-redisStore.js
+++ remoteStorage-redisStore.js
@@ -1,14 +1,11 @@
 var crypto = require('crypto');
-var redis = require('redis').createClient();
+var redis = require('redis-url');
 
 
-redis.on("error", function (err) {
-  console.log("Error " + err);
-});
 
 
 //rs = remotestorage
-function RedisStore(rs) {
+function RedisStore(rs, redisUrl) {
   rs.util.extend(this, rs.util.getEventEmitter('change'));
   this.getPromise = rs.util.getPromise;
   this.asyncEach = rs.util.asyncEach;
@@ -19,6 +16,12 @@ function RedisStore(rs) {
 
   rs.storageAdapter.set(this);
   console.log('INFO: found redis storage');
+
+  this.client = redis.connect(redisUrl);
+  this.client.on("error", function (err) {
+    console.log("Error " + err);
+  });
+
 }
 
 
@@ -26,7 +29,7 @@ RedisStore.prototype = {
 
   get: function(path) {
     var promise = this.getPromise();
-    redis.get(this.path(path), function(err, res) {
+    this.client.get(this.path(path), function(err, res) {
       if (err) console.log('GET Error:', err);
       promise.fulfillLater(res);
     });
@@ -35,7 +38,7 @@ RedisStore.prototype = {
 
   set: function(path, node) {
     var promise = this.getPromise();
-    redis.set(this.path(path), node, this.bind(function(err) {
+    this.client.set(this.path(path), node, this.bind(function(err) {
       if (err) console.log('SET Error:', err);
       this.emit('change', {path: path});
       promise.fulfillLater();
@@ -45,7 +48,7 @@ RedisStore.prototype = {
 
   remove: function(path) {
     var promise = this.getPromise();
-    redis.del(this.path(path), function(err) {
+    this.client.del(this.path(path), function(err) {
       if (err) console.log('DEL Error:', err);
       promise.fulfillLater();
     });
@@ -54,13 +57,13 @@ RedisStore.prototype = {
 
   forgetAll: function() {
     var promise = this.getPromise();
-    redis.keys(this.prefix + '*', this.bind(function(err, res) {
+    this.client.keys(this.prefix + '*', this.bind(function(err, res) {
       if (err) console.log('KEYS Error:', err);
-      this.asyncEach(res, deleteKey).then(function() {
+      this.asyncEach(res, this.bind(deleteKey)).then(function() {
         promise.fulfill();
       });
       function deleteKey(key) {
-        redis.del(key, function(err) {
+        this.client.del(key, function(err) {
           if (err) console.log('DEL Error:', err);
         });
       }
@@ -77,6 +80,6 @@ RedisStore.prototype = {
 };
 
 
-module.exports = function(remotestorage) {
-  return new RedisStore(remotestorage);
+module.exports = function(remotestorage, redisUrl) {
+  return new RedisStore(remotestorage, redisUrl);
 };
